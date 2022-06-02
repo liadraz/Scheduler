d84382e00d665d3621724890866a1ca38dcddcc9
liad.raz <raz.liad@gmail.com>
2022-06-02 20:30:40 +0300

    api: added methods for scheduler and factory interfaces

--- include/scheduler.hpp
+++ include/scheduler.hpp
@@ -12,8 +12,34 @@
 #ifndef SCHEDULER_HPP
 #define SCHEDULER_HPP
 
+
+//----------------------------------------------------------------------------//
+//	Scheduler Deceleration
+//----------------------------------------------------------------------------//
 class Scheduler:
 {
+public:
+	//
+	// Special Members Constructors
+    Scheduler() = default;
+    ~Scheduler() = default;
+    Scheduler(const Scheduler& other_) = delete;
+    Scheduler& operator=(const Scheduler& other_) = delete;
+
+    //
+    // Main Functionality
+    void Run();
+    void Stop();
+
+    // DESCRIPTION Adds a task to Tasks list
+    void InsertTaskClass();
+    // DESCRIPTION Remove a task from Tasks list
+    void DeleteTaskClass();
+
+    void AddTaskInst();
+    void RemoveTaskInst();
+
+private:
 
 };
 
--- include/tasks_factory.hpp
+++ include/tasks_factory.hpp
@@ -12,8 +12,19 @@
 #ifndef TASKS_FACTORY_HPP
 #define TASKS_FACTORY_HPP
 
+template <typename Base, typename Key, typename Args>
 class Factory:
 {
+public:
+    //
+	// Special Members Constructors
+    Factory() = default;
+    ~Factory() = default;
+    // Uncopiable
+    Factory(const Factory& other_) = delete;
+    Factory& operator=(const Factory& other_) = delete;
+
+private:
 
 };
 
