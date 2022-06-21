222d373ac1e6d95c1c4aed5ccb224b2bbbbeabfc
liad.raz <raz.liad@gmail.com>
2022-06-21 20:56:24 +0300

    code: start to impl the defualt ctor

--- include/scheduler.hpp
+++ include/scheduler.hpp
@@ -15,7 +15,9 @@
 #include <queue>  		// std::priority_queue
 #include <ctime>  		// time_t
 
-#include "uid.hpp"  	// Uid
+#include "tasks_creator.hpp"  	// Creator
+#include "itask.hpp"  	        // ITask
+#include "uid.hpp"  	        // Uid
 
 
 //----------------------------------------------------------------------------//
@@ -23,10 +25,10 @@
 //----------------------------------------------------------------------------//
 
 template <
-	typename T, 
+	typename Task, 
 	typename Args, 
-	typename Container = std::vector<T>, 
-	typename Compare = std::less<T>
+	typename Container = std::vector<Task>, 
+	typename Compare = std::less<Task>
 	>
 class Scheduler:
 {
@@ -43,33 +45,57 @@ public:
     void Run();
     void Stop();
 
-    // DESCRIPTION Add a task to the scheduler. 
+    // DESCRIPTION  Add a task to the scheduler. 
     // T is a pointer to a function
     // Args are the parameters the user can pass to T function
     // interval parameter determines its priority value
-    Uid InsertTaskClass(ITask task_, Args param_, time_t interval_);
+    Uid ScheduleTask(Task task_, Args param_, time_t interval_);
+    void RemoveTask(Uid toRemove_);
 
+    //
+    // DESCRIPTION  Add a user custom task type. 
+    // Creating a tasks creator, option to add tasks while the scheduler is running 
+    Uid InsertTaskClass();
     // DESCRIPTION Remove a task from the scheduler.
-    void RemoveTaskClass();
-
+    void DeleteTaskClass();
 
     //
     // Extra functionality
 
     // DESCRIPTION Obtain how many tasks currently exists in the scheduler.
     size_t Size();
+
     // DESCRIPTION Checks if any tasks exists in scheduler 
     bool IsEmpty();
+
     // DESCRIPTION Delete all tasks in the scheduler
     void Clear();
     
-    // TODO: creating a tasks creator, option to add tasks while the scheduler is running 
-    // void AddTaskInst();
-    // void RemoveTaskInst();
 
 private:
-    std::priority_queue<T, Container, Compare> m_tasks;
+    std::priority_queue<Task, Container, Compare> m_tasks;
+    time_t m_initialTime;
+    Creator* m_creator;
 };
+
+
+
+//----------------------------------------------------------------------------//
+//	Scheduler Definitions
+//----------------------------------------------------------------------------//
+template <typename Task, typename Args, typename Container, typename Compare>
+Scheduler<Task, Args, Container, Compare>::Scheduler() : m_initialTime(0)
+{
+    // Create and Initiate the creator. Adding basic tasks to the scheduler
+    typename Creator<ITask, ITask::TASK_NAME, Args>* creator = new Creator<ITask, ITask::TASK_NAME, Args>();
+
+    creator->ProvideTaskType(ITask::PRINT_MSG, PrintMsg::CreateTaskInst);
+
+}
+
+template <typename Task, typename Args, typename Container, typename Compare>
+Scheduler::~Scheduler()
+{}
 
 
 #endif // SCHEDULER_HPP
