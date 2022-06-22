ec8e56b82909aaeff480b68e9e15c6f88e3961b3
liad.raz <raz.liad@gmail.com>
2022-06-22 14:36:58 +0300

    tests: new file to do unittests on scheduler class

---
+++ tests/scheduler_test.cpp
@@ -0,0 +1,21 @@
+
+//----------------------------------------------------------------------------//
+//					Design Patterns
+//----------------------------------------------------------------------------//
+//
+//	DESCRIPTION		Tests - Scheduler Class
+//	AUTHOR 		    Liad Raz
+//	DATE			22 Jun 2022
+//
+//  CpyCompile      gdp11 -I ./include/ tests/scheduler_test.cpp
+//----------------------------------------------------------------------------//
+
+#include "scheduler.hpp"
+
+
+int main()
+{
+
+
+    return 0;
+}
