cce156aceab9e22c2cec13d4d23952dd9cad72c9
liad.raz <raz.liad@gmail.com>
2022-06-23 20:38:21 +0300

    tests: scheduler has compiled and created

--- tests/scheduler_test.cpp
+++ tests/scheduler_test.cpp
@@ -11,12 +11,16 @@
 //  gdp11 -I ./include/ -I ./tasks/ ./src/uid.cpp tests/scheduler_test.cpp
 //----------------------------------------------------------------------------//
 
+#include <string>
+
 #include "scheduler.hpp"
 
 
 int main()
 {
+    std::cout << "Tests Scheduler" << std::endl;
 
+    Scheduler<ITask, ITask::TaskName, Param> sched;
 
     return 0;
 }
