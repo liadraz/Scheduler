10a30de3a38aeebf22948cf6a27bdb77840d2064
liad.raz <raz.liad@gmail.com>
2022-07-03 20:24:12 +0300

    tests: ScheduleTasks tested. All tasks inserted to the pqueue

--- tests/scheduler_test.cpp
+++ tests/scheduler_test.cpp
@@ -8,7 +8,7 @@
 //	DATE			22 Jun 2022
 //
 //  CpyCompile
-//  gdp11 -I ./include/ -I ./tasks/ ./src/uid.cpp tests/scheduler_test.cpp
+//  gdp11 -I ./include/ -I ./tasks/ ./src/uid.cpp src/itask.cpp src/print_msg.cpp tests/scheduler_test.cpp
 //----------------------------------------------------------------------------//
 
 #include <string>
@@ -20,7 +20,20 @@ int main()
 {
     std::cout << "Tests Scheduler" << std::endl;
 
-    Scheduler<ITask, ITask::TaskName, Param> sched;
+    // Create scheduler
+    Scheduler<ITask::TaskName, Param> sched;
+
+    // Schedule print massage task
+    Param p;
+    Uid taskID = sched.ScheduleTask(ITask::PRINT_MSG, p, 3);
+    Uid taskID1 = sched.ScheduleTask(ITask::PRINT_MSG, p, 1);
+    Uid taskID2 = sched.ScheduleTask(ITask::PRINT_MSG, p, 4);
+
+
+    std::cout << taskID << std::endl;
+    std::cout << taskID1 << std::endl;
+    std::cout << taskID2 << std::endl;
+
 
     return 0;
 }
